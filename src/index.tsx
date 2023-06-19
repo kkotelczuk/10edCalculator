a73766690e2d7fe38cc214bcd2807c7281daa936
kkotelczuk <konrad.kotelczuk@gmail.com>
2023-06-19 09:49:56 +0200

    change back to hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import { aeldariData, aeldariEnhancements } from "./data/aeldari";
 import { rikiData, rikiEnhancements } from "./data/riki";
 import { Calculator } from "./Calculator";
@@ -12,42 +12,47 @@ import { sororitasData, sororitasEnhancements } from "./data/sororitas";
 // { id: number, name: string, models: number, points: number }
 // { id: number, name: string, points: number }
 
-const router = createBrowserRouter([
+const router = createHashRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+    },
+    {
+      path: "/cwe",
+      element: (
+        <Calculator
+          units={aeldariData}
+          enhancements={aeldariEnhancements}
+          title="AELDARI"
+        />
+      ),
+    },
+    {
+      path: "/riki",
+      element: (
+        <Calculator
+          units={rikiData}
+          enhancements={rikiEnhancements}
+          title="CHAOS KNIGHTS"
+        />
+      ),
+    },
+    {
+      path: "/sisters",
+      element: (
+        <Calculator
+          units={sororitasData}
+          enhancements={sororitasEnhancements}
+          title="ADEPTA SORORITAS"
+        />
+      ),
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/cwe",
-    element: (
-      <Calculator
-        units={aeldariData}
-        enhancements={aeldariEnhancements}
-        title="AELDARI"
-      />
-    ),
-  },
-  {
-    path: "/riki",
-    element: (
-      <Calculator
-        units={rikiData}
-        enhancements={rikiEnhancements}
-        title="CHAOS KNIGHTS"
-      />
-    ),
-  },
-  {
-    path: "/sisters",
-    element: (
-      <Calculator
-        units={sororitasData}
-        enhancements={sororitasEnhancements}
-        title="ADEPTA SORORITAS"
-      />
-    ),
-  },
-]);
+    basename: "/index.html",
+  }
+);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
